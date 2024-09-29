48e776914019a0155510b8f1b108e18ecdfc92c7
FAYSi223 <134807221+FAYSi223@users.noreply.github.com>
2024-09-29 18:08:39 +0200

    Update Badges.vue

--- src/components/popouts/profile-popout/Badges.vue
+++ src/components/popouts/profile-popout/Badges.vue
@@ -7,19 +7,17 @@
         v-for="item in badges"
         :key="item.name"
         :style="{ borderColor: item.color }"
+        :title="item.name"
       >
         <img :src="item.iconURL" alt="" />
-        <div class="tooltip">{{ item.name }}</div>
       </div>
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import { Badge } from "@/constants/badges";
 import { PropType } from "vue";
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "Badges",
   props: {
@@ -30,7 +28,6 @@ export default defineComponent({
   },
 });
 </script>
-
 <style scoped lang="scss">
 .badges {
   flex-shrink: 0;
@@ -38,13 +35,6 @@ export default defineComponent({
   transition: 0.2s;
   cursor: default;
   user-select: none;
-
-  .title {
-    font-size: 16px;
-    font-weight: bold;
-    position: relative;
-    z-index: 10;
-  }
 }
 .list {
   display: flex;
@@ -53,8 +43,6 @@ export default defineComponent({
   margin-top: 5px;
   margin-bottom: 5px;
   margin-left: 5px;
-  position: relative;
-  z-index: 1;
 }
 .badge {
   display: flex;
@@ -65,38 +53,9 @@ export default defineComponent({
   padding: 5px;
   height: 23px;
   width: 23px;
-  position: relative;
-  z-index: 1;
-
   img {
     height: 15px;
     width: 15px;
-  }
-
-  .tooltip {
-    position: absolute;
-    bottom: -40px;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #2c2f33;
-    color: #fff;
-    padding: 8px 12px;
-    border-radius: 6px;
-    white-space: nowrap;
-    font-size: 14px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    opacity: 0;
-    visibility: hidden;
-    transition: opacity 0.2s ease, visibility 0.2s ease, transform 0.2s ease;
-    pointer-events: none;
-    max-width: 300px;
-    width: auto;
-  }
-
-  &:hover .tooltip {
-    opacity: 1;
-    visibility: visible;
-    transform: translateX(-50%) translateY(-5px);
   }
 }
 </style>
