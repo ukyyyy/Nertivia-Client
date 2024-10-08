b08336ed745947579b0895c6a5d9b74e91fc3c02
FAYSi223 <134807221+FAYSi223@users.noreply.github.com>
2024-10-08 20:43:47 +0200

    Update GifSearch.vue

--- src/components/picker-area/gif-picker/GifSearch.vue
+++ src/components/picker-area/gif-picker/GifSearch.vue
@@ -2,35 +2,42 @@
   <div class="searches">
     <div
       class="item"
-      v-for="({ preview, url, siteUrl }, i) in items"
+      v-for="(item, i) in items"
       :key="i"
-      @click="onClick(siteUrl)"
+      @click="onClick(item.siteUrl)"
     >
       <video
         class="video"
-        :src="url"
+        :src="item.url"
         autoplay
         loop
         v-if="isWindowFocus"
       ></video>
-      <img class="image" v-else :src="preview" alt="" loading="lazy" />
+      <img class="image" v-else :src="item.preview" alt="" loading="lazy" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { TenorSearch } from "@/services/tenorService";
-import { defineComponent, PropType } from "@vue/runtime-core";
-import { useWindowProperties } from "@/utils/windowProperties";
+import { defineComponent, PropType, ref, onMounted, onBeforeUnmount } from "@vue/runtime-core";
 import { insert } from "text-field-edit";
 
 export default defineComponent({
   props: {
-    items: Array as PropType<TenorSearch[]>,
+    items: {
+      type: Array as PropType<TenorSearch[]>,
+      required: true,
+    },
     inputElement: {
       type: Object as PropType<HTMLTextAreaElement>,
       required: true,
     },
+  },
+  data() {
+    return {
+      isFocused: ref(true),
+    };
   },
   methods: {
     onClick(url: string) {
@@ -38,10 +45,24 @@ export default defineComponent({
       this.inputElement.focus();
       this.$emit("close");
     },
+    handleWindowFocus() {
+      this.isFocused = true;
+    },
+    handleWindowBlur() {
+      this.isFocused = false;
+    },
+  },
+  mounted() {
+    window.addEventListener("focus", this.handleWindowFocus);
+    window.addEventListener("blur", this.handleWindowBlur);
+  },
+  beforeUnmount() {
+    window.removeEventListener("focus", this.handleWindowFocus);
+    window.removeEventListener("blur", this.handleWindowBlur);
   },
   computed: {
     isWindowFocus() {
-      return useWindowProperties().isFocused;
+      return this.isFocused;
     },
   },
 });
